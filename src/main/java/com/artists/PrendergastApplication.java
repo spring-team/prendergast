3888e1785cf230649490b3497981da05d2b50387
Jessica Kerr <jessitron@gmail.com>
2018-03-02 17:59:51 -0600

    Initial commit from Atomist

---
+++ src/main/java/com/artists/PrendergastApplication.java
@@ -0,0 +1,12 @@
+package com.artists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrendergastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrendergastApplication.class, args);
+	}
+}
